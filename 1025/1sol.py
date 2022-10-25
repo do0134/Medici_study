d075608b3d859cd258f9b379fdd2206068841bfa
TonyWoo <do0134@naver.com>
2022-10-25 17:20:54 +0900

    1025 1sol

---
+++ 1025/1sol.py
@@ -0,0 +1,22 @@
+import sys
+
+n = int(input())
+crane = list(map(int,sys.stdin.readline().split()))
+m = int(input())
+box = list(map(int,sys.stdin.readline().split()))
+
+crane.sort(reverse = True)
+box.sort(reverse = True)
+
+if crane[0] < box[0] :
+    print(-1)
+else:
+    cnt = 0
+    while box:
+        cnt += 1
+        for i in range(n):
+            for j in box:
+                if crane[i] >= j:
+                    box.remove(j)
+                    break
+    print(cnt)
